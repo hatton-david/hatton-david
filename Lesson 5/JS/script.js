b68589049d475e87d2757c1b49c6b23bf82008b6
hatton-david <david.hatton@ymail.com>
2020-02-08 12:22:09 -0500

    ...

--- Lesson 5/JS/script.js
+++ Lesson 5/JS/script.js
@@ -2,7 +2,7 @@ const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
-  document.querySelector(".navigation").classList.toggle("responsive");
+document.querySelector(".navigation").classList.toggle("responsive");
 }
 
 const date = document.lastModified;
