65b381c58b42ebdb3e8e6d0fcb0efe9f8e338737
hatton-david <david.hatton@ymail.com>
2020-02-28 00:33:45 -0500

    ...

---
+++ Lesson 8/script.js
@@ -0,0 +1,19 @@
+let update = document.lastModified;
+document.getElementById("date").innerHTML = update;
+
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = mm+'.'+dd+'.'+yyyy;
+document.getElementById("dateTime").textContent = today;
