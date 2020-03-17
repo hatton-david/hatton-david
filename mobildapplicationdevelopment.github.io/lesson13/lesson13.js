5b6fc86f21c9d04215153cb37caabc43c206fe90
hatton-david <59677401+hatton-david@users.noreply.github.com>
2020-03-17 00:15:38 -0400

    ...

--- mobildapplicationdevelopment.github.io/lesson13/lesson13.js
+++
@@ -1,8 +0,0 @@
-const hambutton = document.querySelector(".menu");
-hambutton.addEventListener("click", toggleMenu, false);
-
-function toggleMenu() {
-document.querySelector(".navigation").classList.toggle("responsive");
-}
-
-
