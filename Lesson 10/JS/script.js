9ff6b9cec312659a4b8975e891ac7744eeb9eb92
hatton-david <59677401+hatton-david@users.noreply.github.com>
2020-03-16 23:53:19 -0400

    ...

--- Lesson 10/JS/script.js
+++ Lesson 10/JS/script.js
@@ -8,37 +8,33 @@ document.querySelector(".navigation").classList.toggle("responsive");
 const date = document.lastModified;
  document.getElementById("datetime").textContent = date;
 
+ 
 const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=ce89b74543758b40be58cde73a35fa35';
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
-    console.log(jsObject);
+   
+    document.getElementById('current').textContent = jsObject.weather[0].main; 
+    document.getElementById('temperature').textContent = jsObject.main.temp; 
+    document.getElementById('humidity').textContent = jsObject.main.humidity; 
+    document.getElementById('windspeed').textContent = jsObject.main.windSpeed; 
+   
     const currentTemp = document.querySelector('#temperature');
     const windSpeed = document.querySelector('#windspeed');
     const humidity = document.querySelector('#humidity');
-    document.getElementById('icon').setAttribute('src', imagesrc); 
-
+    const currentW = document.getElementById('#current');
+    
+    currentW.textContent = jsObject.main.temp;
     currentTemp.textContent = jsObject.main.temp;
     windSpeed.textContent = jsObject.wind.speed;
     humidity.textContent = jsObject.main.humidity;
 
 
-    function windChill() {
-        var speedF = Math.pow(windSpeed, 0.16);
-        var windChill = Math.round(35.74 + (0.6215 * currentTemp) - (35.75 * speedF) + (0.4275 * currentTemp * speedF));
-      
-        if (currentTemp <= 50 && windSpeed >= 3) {
-          document.getElementById("windchill").innerHTML = windChill;
-        }
-        else {
-            return 'N/A';
-        }
-      }
-      
-      windChill()
 
+
+
+    
   });
 
  
