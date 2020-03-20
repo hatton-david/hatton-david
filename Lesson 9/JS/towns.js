856d2386448c80323adb21820bc448144c56cb01
hatton-david <59677401+hatton-david@users.noreply.github.com>
2020-03-20 01:16:06 -0400

    ...

--- Lesson 9/JS/towns.js
+++ Lesson 9/JS/towns.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+/*const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 fetch(requestURL)
     .then(function (response) {
     return response.json();
@@ -40,4 +40,4 @@ fetch(requestURL)
       }
   
 }   
-});
+});*/
