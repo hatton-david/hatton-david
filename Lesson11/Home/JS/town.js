052dafc4d90b5cef9039ba9d4c9af2660b092d6b
hatton-david <59677401+hatton-david@users.noreply.github.com>
2020-03-24 03:51:12 -0400

    ...

--- Lesson11/Home/JS/town.js
+++ Lesson11/Home/JS/town.js
@@ -19,6 +19,7 @@ fetch('https://byui-cit230.github.io/weather/data/towndata.json')
       let population = document.createElement('p');
       let rainfall = document.createElement('p')
       let image = document.createElement('img');
+      let alt = document.createElement('img');
       
         h2.textContent = towns[i].name; 
         h3.textContent = towns[i].motto; 
@@ -26,7 +27,7 @@ fetch('https://byui-cit230.github.io/weather/data/towndata.json')
         population.textContent = 'Population: ' + towns[i].currentPopulation;
         rainfall.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
         image.setAttribute('src', '../Lesson11/Home/Images/' + towns[i].photo);
-        image.setAttribute('alt', "picture of: " + h2.textContent);
+        alt.setAttribute('alt', "picture of: " + h2.textContent);
       
       card.appendChild(h2);
       card.appendChild(h3);
@@ -34,6 +35,7 @@ fetch('https://byui-cit230.github.io/weather/data/towndata.json')
       card.appendChild(population);
       card.appendChild(rainfall);
       card.appendChild(image);
+      card.appendChild(alt);
 
       document.querySelector('div.towns').appendChild(card);
       }
