0d9a52c622265aec523629987812fbf0ffb17940
hatton-david <59677401+hatton-david@users.noreply.github.com>
2020-04-08 01:44:47 -0400

    ...

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,6 +1,25 @@
-const hambutton = document.querySelector(".ham");
-hambutton.addEventListener("click", toggleMenu, false);
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4092267&appid=d3f1c58302204f75b88736b9d75edafe&units=imperial";
 
-function toggleMenu() {
-document.querySelector(".navigation").classList.toggle("responsive");
-}
+ fetch(apiURL)
+   .then((response) => response.json())
+   .then((jsObject) => {
+    
+ 
+     let ws = jsObject.wind.speed;
+     let t = jsObject.main.temp;
+     let speedF = Math.pow(ws, 0.16);
+     document.getElementById('current').textContent = jsObject.weather[0].main; 
+     document.getElementById('temperature').textContent = jsObject.main.temp; 
+     document.getElementById('humidity').textContent = jsObject.main.humidity; 
+     document.getElementById('windspeed').textContent = jsObject.wind.speed; 
+    
+     if (t <= 50 && ws >= 3) {
+       var windChill = Math.round(35.74 + (0.6215 * t) - (35.75 * speedF) + (0.4275 * t * speedF)) + "&deg;F";
+     }
+     else {
+       var windChill = "N/A";
+     }
+         document.getElementById('windchill').innerHTML = windChill;
+ 
+ 
+   });
